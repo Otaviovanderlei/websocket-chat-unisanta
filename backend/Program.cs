21cbeeb91a009daef4df276ca1e2262e1498d138
Otavio Vanderlei <otaviovanderlei1743@gmail.com>
2026-09-06 18:09:30 -0300

    feat: add webSocket

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,11 @@
+using System.Net.WebSockets;
+using System.Text;
+
+
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -20,6 +28,42 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseWebSockets();
+
 app.MapControllers();
+
+app.Map("/ws", async context =>
+{
+    if (!context.WebSockets.IsWebSocketRequest)
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        return;
+    }
+
+    using var webSocket =
+        await context.WebSockets.AcceptWebSocketAsync();
+
+    var buffer = new byte[4096];
+
+    while (webSocket.State == WebSocketState.Open)
+    {
+        var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), context.RequestAborted
+        );
+
+        if (result.MessageType == WebSocketMessageType.Close)
+        {
+            await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Conexão encerrada", CancellationToken.None);
+            break;
+        }
+
+        var mensagem = Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+        Console.WriteLine($"Mensagem recebida: {mensagem}");
+
+        var resposta = Encoding.UTF8.GetBytes($"Servidor recebeu: {mensagem}");
+
+        await webSocket.SendAsync(new ArraySegment<byte>(resposta), WebSocketMessageType.Text, true, context.RequestAborted);
+    }
+});
 
 app.Run();
